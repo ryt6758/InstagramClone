34f111054dc614a5e81e7c226a0b209d1cb8e3f7
ryt6758 <tyr0412utx.dall@gmail.com>
2018-12-16 20:10:12 +0900

    ChangePosts/content/string=>text

---
+++ db/migrate/20181216110535_change_column_to_posts.rb
@@ -0,0 +1,9 @@
+class ChangeColumnToPosts < ActiveRecord::Migration[5.2]
+  def up
+  	change_column :posts, :content, :text
+  end
+
+  def down
+  	change_column :posts, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_105700) do
+ActiveRecord::Schema.define(version: 2018_12_16_110535) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2018_12_16_105700) do
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
