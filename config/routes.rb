ee19c368c709fb6e624e117747c3783677c2e150
ryt6758 <tyr0412utx.dall@gmail.com>
2018-12-16 21:57:34 +0900

    CreateControlloer/users/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/" => "posts#index"
+
+  get "users/new" => "users#new"
+
+  get "posts/new" => "posts#new"
+
 end
