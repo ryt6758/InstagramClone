017590e8ac2b33aa69753557df11a94146050a53
ryt6758 <tyr0412utx.dall@gmail.com>
2018-12-22 18:52:58 +0900

    relationship_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :follows, dependent: :destroy
+  has_many :follows, class_name: "Follow",
+  					 foreign_key: "user_id",
+  					 dependent: :destroy
   has_one_attached :avatar
 
   validates :email, presence: true
