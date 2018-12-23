19c986dc91a7f265f5b27cb173714ce08f9b1590
TomoakiTANAKA <tanaka1987@gmail.com>
2018-12-23 14:02:55 +0900

    コメントを追加（おもにスタイルベース）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
 	
+	# 全体的なこと
+	# Ruby（というかプログラミング全体的は話）では、tabを使わず、半角スペースを使うことが多い
+	# エディタのデフォルト設定で、tab区切りになっていると思われるので、設定含め要修正
+	# （tabインデントが環境依存になり崩れやすいため）
+
 	before_action :set_current_user
 
 	def  set_current_user
 		@current_user = User.find_by(id: session[:user_id])
 	end
 
+	# @memo 名付けが悪い。checkでは一体何をチェックしているのか明確ではない？
+	# すでにログインしているか？
+	# ユーザーが存在するか？
+	# など複数の意味が想定されてしまう
 	def login_check
 		if @current_user == nil
 			flash[:sucsess] = "ログインまたは会員登録をしてください"
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
 
 	before_action :login_check
 
+	# 半角とtabが混ざっています
 	def create
 	    @like = Like.create(user_id: @current_user.id, post_id: params[:post_id])
 	    redirect_to "/posts/#{params[:post_id]}"
