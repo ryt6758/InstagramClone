058f487028cf0c2a0c4687f24ab2e8be42fb0b66
ryt6758 <tyr0412utx.dall@gmail.com>
2019-01-04 21:29:17 +0900

    followerアソシエーションの構築

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :follows, class_name: "Follow",
-            foreign_key: "user_id",
-            dependent: :destroy
+  has_many :user_follows, class_name: "Follow", dependent: :destroy
+  has_many :followings, through: :user_follows
+  has_many :user_followers, class_name: "Follow", foreign_key: "followed_user_id"
+  has_many :followers, through: :user_followers
   has_one_attached :avatar
 
   validates :email, presence: true
