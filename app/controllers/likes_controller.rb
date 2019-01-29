541f47a8dabe912d097dc8f72f1dc3f021083974
ryt6758 <tyr0412utx.dall@gmail.com>
2019-01-29 22:37:31 +0900

    ajaxにてcountUp/countDownの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,21 +6,14 @@ class LikesController < ApplicationController
     logger.debug("create呼ばれたよ")
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
 
-    # json_response = {
-    #   likes_count: Like.all.length,
-    # }
-
-    # render :json => json_response
-
     respond_to do |format|
       if @like.save
-        json_response = {
+        @json_response = {
           likes_count: Like.where(post_id: params[:post_id]).length,
           most_recently_liked_post: 3, # 一番最近likeされたpostを一緒に返す
         }
-
         format.html { redirect_to "/posts/#{params[:post_id]}" }
-        format.json { render :json => json_response }
+        format.json { render :json => @json_response }
       else
         format.html { render :new }
         format.json { render json: @like.errors, status: :unprocessable_entity }
@@ -30,9 +23,20 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    logger.debug("destroy呼ばれたよ")
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
-    @like.destroy
-	  redirect_to "/posts/#{params[:post_id]}"
+    respond_to do |format|
+      if @like.destroy
+        @json_response = {
+          likes_count: Like.where(post_id: params[:post_id]).length,
+        }
+        format.html { redirect_to "/posts/#{params[:post_id]}" }
+        format.json { render :json => @json_response }
+      else
+        format.html { render :new }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
 end
